ffd619b84c5559c06da4379d594584b8517610df
Mylene Miral <mylenemiral01@gmail.com>
2025-08-30 10:28:54 +0800

    Exer_03

---
+++ MIRAL_COConstructorUserInput/MIRAL_COConstructorUserInput/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_COConstructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //MYLENE I. MIRAL
+        //IT306
+        //EXER_03: User Input with Constructor
+
+        Main:
+            Console.Clear();
+            Console.WriteLine("This program will ask about your favorite shirt.\n");
+            Console.Write("Enter a brand of shirt: \t");
+            string aBrand=Console.ReadLine();
+            Console.Write("Enter a color of shirt: \t");
+            string aColor=Console.ReadLine();
+            Console.Write("Eneter a size:    \t\t");
+            string aSize=Console.ReadLine();
+
+            //
+            Shirt shirt1 = new Shirt(aBrand, aColor, aSize);
+
+            //
+            shirt1.DisplayShirtDetails();
+
+            //
+            Console.Write("\n Do you want to try again (y/n)? ");
+            char ans=Convert.ToChar(Console.ReadLine());
+            if (ans == 'y')
+            {
+                goto Main;
+            }
+            else if(ans == 'n')  
+           {
+                return;
+           }
+
+            
+            
+        }
+    }
+}
---
+++ MIRAL_COConstructorUserInput/MIRAL_COConstructorUserInput/Shirt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_COConstructorUserInput
+{
+    internal class Shirt
+    {
+        //
+        public string brand;
+        public string color;
+        public string size;
+
+        public Shirt(string aBrand,string aColor,string aSize)
+        {
+            brand = aBrand;
+            color = aColor;
+            size = aSize;
+        }
+        public void DisplayShirtDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("----shirt details Area----");
+            Console.WriteLine("Brand Name: \t" + brand);
+            Console.WriteLine("Color: \t\t" + color);
+            Console.WriteLine("Size: \t\t" + size);
+
+        }
+
+
+    }
+}
