a8e60bc98e895a6290a939d105194ae236b9cb39
Mylene Miral <mylenemiral01@gmail.com>
2025-08-30 10:22:18 +0800

    Exer_01

---
+++ MIRAL_CaO/MIRAL_CaO/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_CaO
+{
+    internal class Person
+    {
+        //Class Members: 
+        public string name;
+        public int age;
+        public string gender;
+
+        // 
+        public void Greet() 
+        { 
+            Console.WriteLine(" Hi everyone good day, I'm " + name + " "+ age  + " years old and I'm a " + gender + " person.");
+        }
+
+    }
+}
---
+++ MIRAL_CaO/MIRAL_CaO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_CaO
+{
+    // Mylene I. Miral
+    // IT306
+    // Creating a class 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.name = "Mylene I. Miral";
+            person1.age = 18;
+            person1.gender = "female";
+
+            //call the method
+            person1.Greet();
+            Console.ReadKey();
+
+
+        }
+    }
+}
