b10d54bfb9e2511e2163892f7549ed05d43f3272
Mylene Miral <mylenemiral01@gmail.com>
2025-08-30 10:24:11 +0800

    Exer_02

---
+++ MIRAL_MyClassAndObjects/MIRAL_MyClassAndObjects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_MyClassAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MYLENE I. MIRAL
+            //IT306
+            //Create a Class
+
+
+            //instantiation an object 
+            Shirt shirt1 = new Shirt();
+           Shirt shirt2 = new Shirt();
+
+            //Initialize the value of the fields
+            shirt1.brand = "Calvin Klein";
+            shirt1.color = "White";
+            shirt1.size = "s";
+
+            shirt2.brand = "Lacoste";
+            shirt2.color = "Black";
+            shirt2.size = "xs";
+
+            //Call the method to diplay information bellow
+            Console.WriteLine("These are my favorite shirt: ");
+            shirt1.DisplayInfo();
+            shirt2.DisplayInfo();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MIRAL_MyClassAndObjects/MIRAL_MyClassAndObjects/Shirt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_MyClassAndObjects
+{
+    internal class Shirt
+    {
+        //decalaration variable
+        public string brand;
+        public string color;
+        public string size;
+
+        //method we use
+        public void DisplayInfo()
+        {
+            Console.WriteLine("\nBrand: "+ brand + "\nColor: "  + color + "\nSize: " +  size);
+        }
+    }
+}
