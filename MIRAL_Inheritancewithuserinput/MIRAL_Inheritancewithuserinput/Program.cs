a82053c38a3d921a5488290229fa3a588caf7ddf
Mylene Miral <mylenemiral01@gmail.com>
2025-09-10 19:56:49 +0800

    Add files via upload
    Exer_08

---
+++ MIRAL_Inheritancewithuserinput/MIRAL_Inheritancewithuserinput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_Inheritancewithuserinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Shirt do you want");
+                Console.WriteLine("1.PoloShirt or 2.Casual Shirt");
+            Console.WriteLine("Enter a Choice (1 or 2):");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Write("Enter the brand do you want: ");
+                string brand= Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ MIRAL_Inheritancewithuserinput/MIRAL_Inheritancewithuserinput/Shirt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_Inheritancewithuserinput
+{
+    class Shirt
+    { 
+      public string Brand { get; set; }
+      public string Price { get; set; }
+
+      public void ShirtInfo()
+        {
+            Console.WriteLine("----Shirt Information----");
+            Console.WriteLine("Brandname: " + Brand);
+            Console.WriteLine("Price: " + Price);
+        }
+    }
+
+    class Formal : Shirt
+    {
+        public string Color { get; set; }   
+
+        public void FormalInfo()
+        {
+            ShirtInfo();
+            Console.WriteLine("Color: " + Color);
+        }
+    }
+
+    class Casual : Shirt
+    {
+        public string Size { get; set; }
+
+        public void CasualInfo()
+        {
+            ShirtInfo();
+            Console.WriteLine("Size: " + Size);
+        }
+    }
+
+}
