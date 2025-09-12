a426e29e576078e2c87c0d3fe795fe771cf1d328
Mylene Miral <mylenemiral01@gmail.com>
2025-09-12 10:35:55 +0800

    Exer_08

---
+++ MIRAL_Inheritanceuserinput/MIRAL_Inheritanceuserinput/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_Inheritanceuserinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Choose a type of shirt that you want to buy");//user can choose what type of shirt they want 
+            Console.WriteLine("\t(1)Formal     (2)Casual\n");
+            Console.Write("Type of shirt: ");
+            char ans = Convert.ToChar(Console.ReadLine());
+            if (ans == '1')
+            {
+                Console.WriteLine("\n-----Formal Shirt Information-----");
+                Console.Write("Enter a Brandname: \t");
+                string aBrand = Console.ReadLine();
+                Console.Write("Enter a Price:  \t");
+                int aPrice = int.Parse(Console.ReadLine());
+                Console.Write("Enter a Size:   \t");
+                string aSize = Console.ReadLine();
+                Console.Write("Enter a Color:  \t");
+                string aColor = Console.ReadLine();
+
+                Formal myformal = new Formal(aBrand, aPrice, aSize, aColor);//Instantiate the Formalshirt
+
+                myformal.FormalDisplayInfo();//call the mmethod to display formal details
+
+            }
+            else if (ans == '2')
+            {
+                Console.WriteLine("\n-----Casual Shirt Information-----");
+                Console.Write("Enter a Brandname: \t");
+                string aBrand = Console.ReadLine();
+                Console.Write("Enter a Price:  \t");
+                int aPrice = int.Parse(Console.ReadLine());
+                Console.Write("Enter a Fabric Type: \t");
+                string aFabrictype = Console.ReadLine();
+                Console.Write("Enter a Shirt Lenght: \t");
+                string aShirtlenght = Console.ReadLine();
+
+                Casual mycasual = new Casual(aBrand, aPrice, aFabrictype, aShirtlenght);//Instantiate the Casualshirt
+
+                mycasual.CasualDisplayInfo();//Call the method to display casual info
+            }
+            Console.WriteLine();
+            Console.Write("\nDo you want to try again(y/n): ");//ask the user if they want to try again
+            char ans1 = Convert.ToChar(Console.ReadLine());
+            if (ans1 == 'y')
+            {
+                goto Main;
+            }
+            else if  (ans1 == 'n')
+            {
+                return;
+            }
+              
+        }
+    }
+}
---
+++ MIRAL_Inheritanceuserinput/MIRAL_Inheritanceuserinput/Shirt.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_Inheritanceuserinput
+{
+    class Shirt//Base class
+    {
+        //Instannce of a Shirt class
+        public string Brand;
+        public int Price;
+        //Constructor
+        public Shirt(string brand, int price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+        public void ShirtDisplayInfo()//Ceate a method to display shirt Details
+        {
+            Console.WriteLine("Brandname: \t" + Brand);
+            Console.WriteLine("Price: \t\t" + Price);
+        }
+    }
+    class Formal : Shirt//Derived class
+    {
+        //instance of a formal class
+        public string Size;
+        public string Color;
+        //Constructor
+        public Formal(string brand, int price, string size, string color) : base(brand, price)
+        {
+            Brand = brand;
+            Price = price;
+            Size = size;
+            Color = color;
+        }
+        public void FormalDisplayInfo()//Create a method to display formal Details
+        {
+            Console.Clear();
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("\n----- Formal Shirt Details-----");
+            ShirtDisplayInfo();
+            Console.WriteLine("Size: \t\t" + Size);
+            Console.Write("Color: \t\t" + Color);
+        }
+    }
+    class Casual : Shirt //Derived Class
+    {
+        //Instance of a Casual class
+        public string Fabrictype;
+        public string Shirtlenght;
+        //Constructor
+        public Casual(string brand, int price, string fabrictype, string shirtlenght) : base(brand, price)
+        {
+            Brand = brand;
+            Price = price;
+            Fabrictype = fabrictype;
+            Shirtlenght = shirtlenght;
+        }
+        public void CasualDisplayInfo()//Create a method to display Casual Details
+        {
+            Console.Clear();
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("\n----- Casual Shirt Details-----");
+            ShirtDisplayInfo();
+            Console.WriteLine("Fabric Type: \t" + Fabrictype);
+            Console.Write("Shirt Lenght: \t" + Shirtlenght);
+        }
+    }
+}
